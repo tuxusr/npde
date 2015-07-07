8df9d6657c888400532c4828f8910d894401043e
tuxusr <tuxusr@mail.com>
2015-07-07 15:05:36 -0300

    Estrutura inicial

---
+++ inc/proj.php
@@ -0,0 +1,12 @@
+<?
+/**
+ * INFORMAÇÕES DO PROJETO
+ *
+ * mesmo sistema do antigo portaldoeleitor.com
+ * talvez, restrito a algum/alguns municípios - menor abrangência, maior qualidade
+ * nos comentários, usar o titulo "o que se fala no facebook.com sobre este candidato"
+ * em cada perfil, um questionário com perguntas como "vc conhece este candidato? irá votar nele? votaria nele? porque (opcional)?" usando recurso de login com facebook
+ * notícias retiradas de diretórios municipais, TRE e sites de notícias locais
+ * opções de propaganda para candidatos e empresas patrocinadoras
+ */
+
